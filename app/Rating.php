d68ba6c94610cde732b29ade9d66f4cee3a6a969
James Benner <james.benner@gmail.com>
2016-02-20 20:48:04 -0500

    Added "Ratings" migration.

---
+++ app/Rating.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Rating
+ *
+ * @property integer $id
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @property string $name
+ * @property string $description
+ * @property integer $organization_id
+ */
+class Rating extends Model
+{
+    //
+}
