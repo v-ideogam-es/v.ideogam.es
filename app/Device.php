180af22b4f49133cdf0d7a9112c7140bd3097161
James Benner <james.benner@gmail.com>
2016-02-20 20:41:41 -0500

    Adding "Devices" table and model.

---
+++ app/Device.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Device
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $description
+ * @property float $price
+ * @property string $upc_a
+ * @property integer $organization_id
+ * @property integer $platform_id
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ */
+class Device extends Model
+{
+    //
+}
---
+++ database/migrations/2016_02_19_223212_create_devices_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDevicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('devices', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->index();
+            $table->text('description')->nullable();
+            $table->decimal('price', 6, 2)->nullable();
+            $table->string('upc_a', 12)->nullable();
+            $table->integer('organization_id')->unsigned();
+            $table->foreign('organization_id')->references('id')->on('organizations');
+            $table->integer('platform_id')->unsigned();
+            $table->foreign('platform_id')->references('id')->on('platforms');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('devices');
+    }
+}
