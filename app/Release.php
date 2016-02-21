c299fc5f1641215501682404e6a4280c6101e3ed
James Benner <james.benner@gmail.com>
2016-02-20 20:48:24 -0500

    Added "Releases" migration.

---
+++ app/Release.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Release
+ *
+ * @property integer $id
+ * @property string $upc_a
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ */
+class Release extends Model
+{
+    //
+}
