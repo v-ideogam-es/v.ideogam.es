42a2e20b5d4af6585d0da05448b9ec874a3ad0b5
James Benner <james.benner@gmail.com>
2016-02-20 20:31:24 -0500

    Added `GameScope` class.

---
+++ app/Scopes/GameScope.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Scopes;
+
+use Illuminate\Database\Eloquent\Scope;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+
+class GameScope implements Scope
+{
+    /**
+     * Apply the scope to a given Eloquent query builder.
+     *
+     * @param Builder $builder
+     * @param Model   $model
+     * @return void
+     */
+    public function apply(Builder $builder, Model $model)
+    {
+        return $builder->where('type', 'application');
+    }
+}
