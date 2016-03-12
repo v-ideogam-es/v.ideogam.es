c950ce1124fee2346f1f56097540147290cf9550
James Benner <james.benner@gmail.com>
2016-03-12 14:22:15 -0500

    Add Semantic UI into the app layout.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,8 +7,10 @@
         <meta name="author" content="@yield('author')">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta property="dcterms:modified" content="@yield('timestamp')">
-        <title>@yield('title')</title>
+        <title>@yield('title', 'v.ideogam.es')</title>
         <link rel="canonical" href="@yield('canonical_url')">
+        <link rel="stylesheet" href="//oss.maxcdn.com/semantic-ui/2.1.8/semantic.min.css">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body>
         @yield('content')
