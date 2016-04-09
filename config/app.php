5c88c2bde966d2e19bbcdb7202c8402d2b459ee0
James Benner <james.benner@gmail.com>
2016-04-08 20:05:32 -0400

    Add `Flash` service provider.

--- config/app.php
+++ config/app.php
@@ -162,6 +162,7 @@ return [
         Barryvdh\Debugbar\ServiceProvider::class,
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
+        Laracasts\Flash\FlashServiceProvider::class,
         Laravolt\Avatar\ServiceProvider::class,
 
     ],
@@ -215,6 +216,7 @@ return [
          */
         'Avatar'    => Laravolt\Avatar\Facade::class,
         'Debugbar'  => Barryvdh\Debugbar\Facade::class,
+        'Flash'     => Laracasts\Flash\Flash::class
 
     ],
 
