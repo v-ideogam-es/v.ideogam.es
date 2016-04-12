e4d998fad2edc67389d659cf44f1574dd30d0395
James Benner <james.benner@gmail.com>
2016-04-11 21:45:46 -0400

    Adjusted the active nav item logic.

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,7 +1,7 @@
 <nav class="ui menu">
     <div class="header item">v.ideogam.es</div>
     @foreach (['device', 'game', 'organization', 'release'] as $resource)
-        @if (request()->path() === $resource)
+        @if (current(request()->segments()) === $resource)
             <a class="active item" href="{{ route($resource . '.index') }}">{{ ucfirst(str_plural($resource)) }}</a>
         @else
             <a class="item" href="{{ route($resource . '.index') }}">{{ ucfirst(str_plural($resource)) }}</a>
